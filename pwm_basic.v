c8951fead14a4333bd5dd8b2f71da10dcf3bb245
aryan3ten <167566276+aryan3ten@users.noreply.github.com>
2026-07-11 23:06:26 +0530

    Update pwm_basic.v

--- pwm_basic.v
+++ pwm_basic.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/01/2026 05:19:43 PM
-// Design Name: 
-// Module Name: pwm_basic
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module pwm_basic
 #(parameter BITS=8)(  
       input clk,
