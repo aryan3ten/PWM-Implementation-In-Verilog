41c69a0de3c9b4a5b3715a897538f54a6bd8fabb
aryan3ten <167566276+aryan3ten@users.noreply.github.com>
2026-07-12 18:11:51 +0530

    Update timer_input.v

--- timer_input.v
+++ timer_input.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/01/2026 09:15:37 PM
-// Design Name: 
-// Module Name: timer_input
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module timer_input
   #(parameter BITS = 8)(
   input clk,
